3677c924176bd0e35a758c58a4ea134349a8078e
Princemashumu <113059693+Princemashumu@users.noreply.github.com>
2024-10-22 10:42:52 +0200

    Added skills

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Heading, Text, Link, HStack, Tooltip } from '@chakra-ui/react';
+import { Box, Heading, Text, Link, HStack, Tooltip} from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaGithub, FaWhatsapp } from 'react-icons/fa';
-import { SiHtml5, SiCss3, SiJavascript, SiReact, SiRedux, SiMongodb, SiCplusplus, SiSqlite, SiFigma, SiVisualstudio, SiAffinitydesigner } from 'react-icons/si';
+import { SiHtml5, SiCss3, SiJavascript, SiReact, SiRedux, SiCplusplus, SiSqlite, SiFigma, SiVisualstudio} from 'react-icons/si';
 import Lottie from 'lottie-react'; // Import Lottie component
-import animationData1 from '../assets/anime4.json'; // Import your Lottie animation JSON file
+import animationData1 from '../assets/anime1.json'; // Import your Lottie animation JSON file
 import animationData2 from '../assets/anime3.json';
 import Splash from '../components/Splash';
 
@@ -32,10 +32,12 @@ const Home = () => {
           loop
           style={{
             position: 'absolute',
-            top: '80%',
-            left: '10%',
+            top: '77%',
+            left: '88%',
             width: '50%',
             height: '50%',
+            zIndex:'1',
+            color:'purple',
             transform: 'translate(-50%, -50%)',
           }}
         />
@@ -49,7 +51,7 @@ const Home = () => {
           display="flex"
           flexDirection="column"
           alignItems="center"
-          gap={4}
+          gap={5}
         >
           {/* Social Links */}
           {[
@@ -68,7 +70,7 @@ const Home = () => {
               _hover={{ color: 'teal.500', bg: 'purple.600' }}
               p={2}
               border="1px solid white"
-              borderRadius="8px"
+              borderRadius="25px"
               bg="#800080"
             >
               {social.icon}
@@ -77,58 +79,60 @@ const Home = () => {
         </Box>
 
         {/* Content */}
-        <Box
-          position="relative"
-          zIndex={1}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          flexDirection="column"
-          color="white"
-          textAlign="center"
-          height="100%"
-        >
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <MotionBox
-              initial={{ x: '-100vw' }}
-              animate={{ x: 0 }}
-              transition={{ type: 'spring', stiffness: 50, duration: 2 }}
-              mr={2}
-            >
-              <Heading as="h1" size="2xl" mb={5} display="inline-block"></Heading>
-            </MotionBox>
-            <MotionBox
-              initial={{ x: '100vw' }}
-              animate={{ x: 0 }}
-              transition={{ type: 'spring', stiffness: 15, duration: 5 }}
-            >
-              <Heading as="h1" size="2xl" mb={5} display="inline-block">
-                Digital Experiences, One Line of Code at a Time
-              </Heading>
-            </MotionBox>
-          </Box>
+<Box
+  position="relative"
+  zIndex={1}
+  display="flex"
+  alignItems="flex-start" // Change this to flex-start to align items to the left
+  justifyContent="flex-start" // Change this to flex-start to position content on the left
+  flexDirection="column"
+  color="white"
+  textAlign="left" // Set text alignment to left
+  height="100%"
+  top="32%"
+  paddingLeft="10%" // Optional: Add padding to give some space from the left edge
+>
+  <Box display="flex" justifyContent="flex-start" alignItems="center" spacing='6'>
+    <MotionBox
+      initial={{ x: '-100vw' }}
+      animate={{ x: 0 }}
+      transition={{ type: 'spring', stiffness: 50, duration: 2 }}
+      mr={2}
+    >
+      <Heading as="h1" size="2xl" mb={5} display="inline-block"></Heading>
+    </MotionBox>
+    <MotionBox
+      initial={{ x: '100vw' }}
+      animate={{ x: 0 }}
+      transition={{ type: 'spring', stiffness: 15, duration: 5 }}
+    >
+      <Heading as="h1" size="2xl" mb={5} display="inline-block" color="white">
+        <Text as="span" color="#800080">Digital</Text> Solutions <br /> One Line of Code at a Time
+      </Heading>
+    </MotionBox>
+  </Box>
 
-          <Text fontWeight="bold" fontSize="xl" mb={5}>
-            <Text as="span" color="#800080">Turning</Text> ideas into interactive digital experiences with a touch of{' '}
-            <Text as="span" color="#800080">code</Text>
-          </Text>
+  <Text fontWeight="bold" fontSize="xl" mb={5}>
+    <Text as="span" color="white">Turning</Text> ideas into interactive digital experiences with a touch of{' '}
+    <Text as="span" color="#800080">code.</Text>
+  </Text>
 
-          <Text
-            as={motion.div}
-            initial={{ opacity: 0, y: 30 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5 }}
-            fontWeight="light"
-            fontSize="60px"
-            color="black"
-            mb={5}
-            fontFamily="'Nabla', cursive"
-            _hover={{ transform: 'translateY(-10px)' }}
-          >
-            DIGITAL SOLUTIONS DEVELOPER
-          </Text>
-        </Box>
-      </Box>
+  <Text
+    as={motion.div}
+    initial={{ opacity: 0, y: 30 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.5 }}
+    fontWeight="light"
+    fontSize="60px"
+    color="white"
+    mb={5}
+    fontFamily="heading"
+    _hover={{ transform: 'translateY(-10px)' }}
+  >
+    DIGITAL SOLUTIONS DEVELOPER
+  </Text>
+</Box>
+</Box>
 
       {/* Skills Section inside the second Lottie Animation */}
       <Box backgroundColor="black" position="relative" height="100vh" overflow="hidden">
@@ -136,41 +140,132 @@ const Home = () => {
           animationData={animationData2}
           loop
           style={{
-            position: 'absolute',
-            top: '38%',
-            left: '88%',
+            position: 'fixed',
+            top: '35%',
+            left: '22%',
             width: '50%',
             height: '50%',
             transform: 'translate(-50%, -50%)',
           }}
         />
+        <Box
+  position="relative"
+  zIndex={1}
+  display="flex"
+  alignItems="flex-start" // Change this to flex-start to align items to the left
+  justifyContent="flex-start" // Change this to flex-start to position content on the left
+  flexDirection="column"
+  color="white"
+  textAlign="left" // Set text alignment to left
+  height="100%"
+  top="32%"
+  paddingLeft="10%" // Optional: Add padding to give some space from the left edge
+>
+  <Box display="flex" justifyContent="flex-start" alignItems="center" spacing='6'>
+    <MotionBox
+      initial={{ x: '-100vw' }}
+      animate={{ x: 0 }}
+      transition={{ type: 'spring', stiffness: 50, duration: 2 }}
+      mr={2}
+    >
+      <Heading as="h1" size="2xl" mb={5} display="inline-block"></Heading>
+    </MotionBox>
+    <MotionBox
+      initial={{ x: '100vw' }}
+      animate={{ x: 0 }}
+      transition={{ type: 'spring', stiffness: 15, duration: 5 }}
+    >
+      <Heading as="h1" size="2xl" mb={5} display="inline-block" color="white">
+        <Text as="span" color="#800080">Skills &</Text> Experiences <br />
+      </Heading>
+    </MotionBox>
+  </Box>
+
+  <Text fontWeight="light" fontSize="large" mb={5}>
+    <Text as="span" color="white">"As a passionate Digital Solutions Developer <br></br> 
+    I transform ideas into interactive and captivating digital experiences, <br></br> 
+     one line of code at a time. With a strong foundation in HTML, CSS, <br></br> and JavaScript,  
+     I bring web interfaces to life, while my expertise in <br></br> React and Redux ensures seamless,
+    dynamic user experiences.<br></br>  I dive deep into backend development with MongoDB and SQL, 
+      and <br></br> enhance user interfaces with tools like Figma.<br></br>  From crafting elegant code to solving complex challenges,<br></br> 
+      I build solutions that not only meet expectations but exceed them,<br></br> 
+     making technology both powerful and beautifully simple."</Text>
+    <Text as="span" color="#800080" fontWeight="bold"><br></br>-Prince Ngwako Mashumu.</Text>
+  </Text>
+  </Box>
+  
+
 
         {/* Skills Section */}
-        <Box position="relative" top="85%" left="50%" transform="translate(-50%, -50%)" color="white">
-          <HStack spacing={8} wrap="wrap" justifyContent="center" mb={10}>
-            {[
-              { icon: <SiHtml5 size="80" color="#E34F26" />, label: 'HTML' },
-              { icon: <SiCss3 size="80" color="#1572B6" />, label: 'CSS' },
-              { icon: <SiJavascript size="80" color="#F7DF1E" />, label: 'JavaScript' },
-              { icon: <SiReact size="80" color="#61DAFB" />, label: 'ReactJS' },
-              { icon: <SiRedux size="80" color="#764ABC" />, label: 'Redux' },
-              { icon: <SiMongodb size="80" color="#47A248" />, label: 'MongoDB' },
-              { icon: <SiCplusplus size="80" color="#00599C" />, label: 'C++' },
-              { icon: <SiSqlite size="80" color="#003B57" />, label: 'SQL' },
-              { icon: <SiFigma size="80" color="#F24E1E" />, label: 'Figma' },
-              { icon: <SiVisualstudio size="80" color="#5C2D91" />, label: 'Visual Studio Code' },
-              { icon: <SiAffinitydesigner size="80" color="#A179B4" />, label: 'Affinity Designer' },
-            ].map((skill, index) => (
-              <Tooltip key={index} label={skill.label} fontSize="md">
-                <Box display="flex" flexDirection="column" alignItems="center">
-                  {skill.icon}
-                  <Text fontSize="sm" color="white" textAlign="center">{skill.label}</Text>
-                </Box>
-              </Tooltip>
-            ))}
-          </HStack>
+
+        <Box border="1px solid white"
+              borderRadius="50px"
+              bg="#800080"
+              zindex="1"
+              position="absolute" 
+              top="65%" 
+              left="72%" 
+              transform="translate(-50%, -50%)" 
+              color="white"
+              paddingTop="40px">
+  {/* Top Row */}
+  <HStack spacing={6} wrap="wrap" justifyContent="center" mb={2}>
+    {[
+      { icon: <SiHtml5 size="80" color="#E34F26" />, label: 'HTML' },
+      { icon: <SiCss3 size="80" color="#1572B6" />, label: 'CSS' },
+      { icon: <SiJavascript size="80" color="#F7DF1E" />, label: 'JavaScript' },
+      // { icon: <SiMongodb size="80" color="#47A248" />, label: 'MongoDB' },
+     
+    ].map((skill, index) => (
+      <Tooltip key={index} label={skill.label} fontSize="md">
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          p={1} // Padding for top icons
+          m={1} // Slight margin to space icons slightly
+          width="100px"
+          height="100px"
+        >
+          {skill.icon}
+          <Text fontSize="sm" color="white" textAlign="center">{skill.label}</Text>
         </Box>
+      </Tooltip>
+    ))}
+  </HStack>
+
+  {/* Bottom Row */}
+  <HStack spacing={6} wrap="wrap" justifyContent="flex-end" mb={10}>
+    {[
+      { icon: <SiCplusplus size="80" color="#00599C" />, label: 'C++' },
+      { icon: <SiSqlite size="80" color="#003B57" />, label: 'SQL' },
+      { icon: <SiFigma size="80" color="#F24E1E" />, label: 'Figma' },
+      { icon: <SiVisualstudio size="100" color="#5C2D91" />, label: 'Visual Studio' },
+      { icon: <SiReact size="80" color="#61DAFB" />, label: 'ReactJS' },
+      { icon: <SiRedux size="80" color="#764ABC" />, label: 'Redux' },
+     
+    ].map((skill, index) => (
+      <Tooltip key={index} label={skill.label} fontSize="md">
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          p={2} // Increased padding for bottom icons
+          m={1} // Slight margin to space icons slightly
+          width="100px"
+          height="100px"
+        >
+          {skill.icon}
+          <Text fontSize="sm" color="white" textAlign="center">{skill.label}</Text>
+        </Box>
+      </Tooltip>
+    ))}
+  </HStack>
+</Box>
+
+
       </Box>
+      
     </>
   );
 };
