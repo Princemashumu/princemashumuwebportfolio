98282569c3424cc61f1f33a096949d27ad4ad194
princemashumu <princemashumu@yahoo.com>
2025-01-10 21:53:00 +0200

    feat. routing fixed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Skills from './pages/Skills';
-import Contact from './pages/Contact';
 import Navbar from './components/Navbar';
 
 
@@ -14,9 +11,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
